641fc5b537ac062b9e9ee2fe06fb44b46e09528b
Lerix <185734549@qq.com>
2019-07-19 23:21:43 +0800

    添加投资方法及相关属性

--- contracts/basicFunding.sol
+++ contracts/basicFunding.sol
@@ -6,12 +6,21 @@ contract CrowFunding{
     uint  public  supportBalance;//支持金额
     uint public targetBalance;//目标金额
     uint public endTime;//截止日期
-
+    address[] public investors;//投资人s
+    mapping(address=>bool) public investExistMap;//存储该用户是否投资过本项目，最快速的方式。默认false代表没有投资过本霞荧幕
+  
     constructor(string memory _projectName,uint _supportBalance,uint _targetBalance,uint _durationInSeconds)public{
         creator = msg.sender;
         projectName = _projectName;
         supportBalance = _supportBalance;
         targetBalance = _targetBalance;
-        endTime = now+_durationInSeconds;//按秒
+        endTime = now + _durationInSeconds;//按秒
+    }
+    // 投资
+    function invest()public payable{
+        require(investExistMap[msg.sender]==false, "已经参与过了");
+        require(msg.value==supportBalance, "只能投资固定金额");
+        investors.push(msg.sender);
+        investExistMap[msg.sender] = true;
     }
 }
