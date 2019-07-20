4d2a5b1b9c89dbc178bca1228d7e486369be1590
Lerix <185734549@qq.com>
2019-07-21 02:10:19 +0800

    web3调用成功，工厂合约调用成功

--- funding-eth-react/src/App.js
+++ funding-eth-react/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import web3 from './utils/InitWeb3'
+import contract from './eth/contracts'
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends React.Component{ 
+  constructor(){
+    super()
+    this.state={
+      currentAccount :""
+    }
+  }
+  async componentDidMount(){
+    let accounts = await web3.eth.getAccounts();
+    this.setState({currentAccount:accounts[0]})
+    let fundingArray = await contract.methods.getAllFungdings().call()
+    console.log(fundingArray)
+  }
+  render() {
+    return (
+      <div>
+        您当前的地址为：{this.state.currentAccount}
+      </div>
+    );
+  }
 }
 
 export default App;
