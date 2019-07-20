6ea8ef01fed3a536df3954b6eb38a0c7e31b1549
Lerix <185734549@qq.com>
2019-07-20 12:15:21 +0800

    花费结构申请定义及方法定义

--- contracts/basicFunding.sol
+++ contracts/basicFunding.sol
@@ -38,4 +38,30 @@ contract CrowFunding{
     function getInvestors() public view returns(address payable[] memory){
         return investors;
     }
+    // 申请批准
+    struct Request {
+        string purpose;//用途介绍
+        uint cost;//所需花费
+        address shopAddress; //向谁购买
+        uint voteCount;//赞成票数
+        mapping(address => bool) investorVotedMap;//记录是否已经投过票
+        RequestStatus status;//申请状态
+    }
+    enum RequestStatus {
+        Voting,//投票中
+        Approved,//已批准
+        Completed//已完成
+    }
+    // 存储多个申请
+    Request[] public requests;
+    function createRequest(string  memory _purpose,uint _cost,address _shopAddress) public {
+        Request memory request = Request({
+            purpose : _purpose,
+            cost : _cost,
+            shopAddress : _shopAddress,
+            voteCount : 0,
+            status : RequestStatus.Voting
+        });
+        requests.push(request);
+    }
 }
